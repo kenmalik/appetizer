9823c81de3131d98cb0258bac93aea2be77cb3eb
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-01-08 23:02:59 -0800

    Add rest of application info to info page

--- info/info.go
+++ info/info.go
@@ -1,17 +1,29 @@
 package info
 
 import (
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/kenmalik/appetizer/types"
 )
 
+var (
+	headerStyles = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderBottom(true)
+	linkStyles = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#f5a142")).
+      Underline(true)
+)
+
 type Model struct {
-	Application types.Application
+	application types.Application
 }
 
 func New(application types.Application) Model {
 	return Model{
-		Application: application,
+		application: application,
 	}
 }
 
@@ -20,7 +32,20 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) View() string {
-	return m.Application.Company + " " + m.Application.Position
+	s := headerStyles.Render(fmt.Sprintf("   %s - %s   ", m.application.Company, m.application.Position)) + "\n\n"
+
+	s += m.application.Location + "\n\n"
+
+	s += "Posted: " + m.application.DatePosted + "\n\n"
+
+	s += "Source: " + linkStyles.Render(m.application.Url) + "\n"
+
+  s += "\nStatus: " + m.application.Status + "\n"
+	s += "Applied: " + m.application.DateApplied + "\n"
+
+	s += "\nAdditional Notes:\n" + m.application.Notes
+
+	return s
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
